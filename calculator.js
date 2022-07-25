177e6acb593da9fe1ed7010ef2e5fa15afefa6a1
Tomas Alves de Souza <tomas.alves08@gmail.com>
2022-07-26 09:52:45 +1200

    Link JavaScript file and Create Pseudocode for calculator app

---
+++ calculator.js
@@ -0,0 +1,30 @@
+// 1. Create a sum function
+
+// 2. Create a subtraction function
+
+// 3. Create a multiply function
+
+// 4. Create a division function
+
+// 5. Create a delete function
+// 5.1 Create a function which deletes just the last number entered and
+//     which does not change if there is already a number saved
+//     previously
+
+// 6. Create a reset function
+// 6.1 Create a function which reset all the three variables for number
+//     1, number 2 and arithmetic function variable
+
+// 7. Create a enter function
+// 7.1 Create a function that calls a function which will use both
+//     saved number variables and the arithmetic function variable
+//     as parameters
+
+// 8. Put all functions together
+// 8.1 Make all the arithmetic functions save the first number when
+//     they are clicked in a variable
+// 8.2 Once arithmetic function is clicked it will be saved in a
+//     variable
+// 8.3 Second number will be saved in a third variable
+// 8.4 Enter button will call a function based on these 3 variables
+//     as inputs
